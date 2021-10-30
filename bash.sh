f0924bfa3f6ef0e6a7b8bbcf48673af8b4464dfa
f2f22 <78264718+f2f22@users.noreply.github.com>
2021-10-30 00:01:27 +0000

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
+cat << "EOF"
 
-figlet -c F 2 F | perl -nle 'print' 
-echo "Welcome to My Script" $0
+
+        ░░░░░░███████ ]▄▄▄▄▄▄▄▄ === GHOST ===
+        ▂▄▅█████████▅▄▃▂
+        I███████████████████]. By : F 2 F
+        ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...
+
+
+
+
+EOF
+cf='\033[0m'
+#Red='\033[0;31m'
+BIRed='\033[1;91m'
+Blue='\033[0;34m'
+On_White='\033[47m'
+Cyan='\033[0;36m'
+printf "${BIRed} #### Welcome To GHOST ####\n${cf}" $0
 sleep 1
-read -p "the domains list : " domainslist
+read -p " the domains list : " domainslist
 if [ -z $domainslist ]; then
     echo "the domains list !!"
     exit
 fi
-echo ".... Start Search ...."
+printf "${Blue}${On_White}.... Start Search ....\n${cf}"
 sleep 2 
 for domain in $(cat $domainslist); do
     firefox --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0" &
+    #firefox --new-tab "https://www.facebook.com/$domain" &
+    # thes open in chrome : google-chrome --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0" &
     sleep 2
 done
 #firefox --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0"
+
+
