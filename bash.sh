7abaf99f8e70e1a81b4b676b4d81c24e69b95d76
f2f22 <78264718+f2f22@users.noreply.github.com>
2021-10-30 01:15:05 +0000

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+nocolor="\033[0m"
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+blue="\033[0;34m"
+domain=$1
+
+sleep 3
 cat << "EOF"
 
 
@@ -8,31 +16,27 @@ cat << "EOF"
         I███████████████████]. By : F 2 F
         ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...
 
-
-
-
 EOF
-cf='\033[0m'
-#Red='\033[0;31m'
-BIRed='\033[1;91m'
-Blue='\033[0;34m'
-On_White='\033[47m'
-Cyan='\033[0;36m'
-printf "${BIRed} #### Welcome To GHOST ####\n${cf}" $0
-sleep 1
-read -p " the domains list : " domainslist
-if [ -z $domainslist ]; then
-    echo "the domains list !!"
+
+if [ $(whoami) != "root" ]; then
+    echo -e "${red} You Are Not root${noclor}"
     exit
+else
+if [ -z $domain ]; then
+    echo -e "${red} Enter The Domain${noclor}"
+else
+    
+ echo -e "${green}============ [Start On $domain] ============${noclor}"
+ gnome-terminal -e "ping -c3 $domain"
+ sleep 2
+touch Scan.txt && gnome-terminal -e "tail -F Scan.txt"
+touch ScanNikato.txt && gnome-terminal -e "tail -F Scan.txt"
+nmap -sS -O -oX nmap.xml $domain > Scan.txt
+
+sleep 2
+nikto  -h  $domain > ScanNikato.txt
+sleep 2
+ echo -e "${green}=================== [End] ===================${noclor}"
+
 fi
-printf "${Blue}${On_White}.... Start Search ....\n${cf}"
-sleep 2 
-for domain in $(cat $domainslist); do
-    firefox --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0" &
-    #firefox --new-tab "https://www.facebook.com/$domain" &
-    # thes open in chrome : google-chrome --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0" &
-    sleep 2
-done
-#firefox --new-tab "https://searchdns.netcraft.com/?host=$domain&x=0&y=0"
-
-
+fi
